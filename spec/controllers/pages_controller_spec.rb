9db5d65ac89d565370754d261df908f985e67673
Chad Yehia <cyehia@gmail.com>
2011-04-17 01:56:22 -0400

    another

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,6 +12,7 @@ end
       get 'home'
       response.should be_success
     end
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", :content => "#{@base_title} | Home")
